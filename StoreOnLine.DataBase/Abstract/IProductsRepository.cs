89fe87551d5a8df39355d252e1ea1da61c2c6b90
Pedro Curich <curichpedro@gmail.com>
2014-08-27 01:57:30 -0500

    fffffffffff
    fffffffffffffff

--- StoreOnLine.DataBase/Abstract/IProductsRepository.cs
+++ StoreOnLine.DataBase/Abstract/IProductsRepository.cs
@@ -5,7 +5,13 @@ namespace StoreOnLine.DataBase.Abstract
 {
     public interface IProductsRepository
     {
-        IEnumerable<ProductBase> ProductBases  { get; }
-        IEnumerable<ProductComposite> ProductComposites{ get; }
+        IEnumerable<ProductBase> ProductBases { get; }
+        IEnumerable<ProductComposite> ProductComposites { get; }
+
+        int SaveProductBase(ProductBase product);
+        int SaveProductComposite(ProductComposite product);
+
+        Product DeleteProductBase(int productId);
+        Product DeleteProductComposite(int productId);
     }
 }
