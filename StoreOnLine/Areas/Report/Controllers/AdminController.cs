251c4ffa35a45920a298737a68af2c2d4f0e6111
Pedro Curich <curichpedro@gmail.com>
2014-12-01 12:45:21 -0500

    GNCFGVN
    UJH,K

--- StoreOnLine/Areas/Report/Controllers/AdminController.cs
+++ StoreOnLine/Areas/Report/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +11,12 @@ namespace StoreOnLine.Areas.Report.Controllers
     {
         //
         // GET: /Report/Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult Index()
         {
             return View();
